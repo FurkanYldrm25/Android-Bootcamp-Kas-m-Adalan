020698cc998c50e01e2fce71e5582904cc1ef771
Furkan Yıldırım <frknyldrm2925@gmail.com>
2025-05-01 18:31:45 +0300

    Tasarım Çalışması Ödevi

---
+++ tasarimcalismasi/app/src/main/java/com/example/tasarimcalismasi/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.example.tasarimcalismasi
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        // R: res klasörü içinde kotlin dosyaları içindir.
+        //XML de kullanılan @ işareti res klasörü içinde xml dosyaları içindir.
+        }
+    }
